b163e0a4f718749eb3417b4b63705baf57c7cfb9
July Slavhev <jul.slavchev.tues19@gmail.com>
2020-02-13 16:58:18 +0200

    Sealed class Result added with success and error classes.

---
+++ app/src/main/java/com/example/beer_app_android/Result.kt
@@ -0,0 +1,9 @@
+package com.example.beer_app_android
+
+import java.lang.Exception
+
+sealed class Result<out T : Any> {
+    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+
+}

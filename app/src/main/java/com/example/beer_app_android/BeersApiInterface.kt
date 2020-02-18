03f213212e1d5ae9fc89f915f261b55786c15fa3
July Slavhev <jul.slavchev.tues19@gmail.com>
2020-02-18 16:40:25 +0200

    Function postBeerAsync added to the BeersApiInterface.

--- app/src/main/java/com/example/beer_app_android/BeersApiInterface.kt
+++ app/src/main/java/com/example/beer_app_android/BeersApiInterface.kt
@@ -1,8 +1,11 @@
 package com.example.beer_app_android
 
+import com.google.gson.JsonObject
 import kotlinx.coroutines.Deferred
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface BeersApiInterface {
@@ -10,6 +13,7 @@ interface BeersApiInterface {
     @GET("beer/{id}")
     fun getBeerAsync(@Path("id") id:Int): Deferred<Response<Beer>>
 
-
+    @POST("beer/")
+    fun postBeerAsync(@Body beer: JsonObject): Deferred<Response<Beer>>
 
 }

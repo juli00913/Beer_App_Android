468af25d71cbb98ae8a4f03389edb7a69b5133f7
July Slavhev <jul.slavchev.tues19@gmail.com>
2020-02-18 16:49:18 +0200

    Function postBeer added to BeerViewModel. Convert beer object to jsonObject

--- app/src/main/java/com/example/beer_app_android/BeerViewModel.kt
+++ app/src/main/java/com/example/beer_app_android/BeerViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.beer_app_android
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.gson.JsonObject
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
@@ -17,11 +19,24 @@ class BeerViewModel : ViewModel() {
 
     val beerLiveData = MutableLiveData<Beer>()
 
+    private lateinit var jsonObject: JsonObject
+
     fun getBeer() {
 
         scope.launch {
             val beer = beerRepository.getBeer()
             beerLiveData.postValue(beer)
+        }
+    }
+
+    fun postBeer(beer: Beer) {
+
+        jsonObject = JsonObject()
+        jsonObject.addProperty("name", beer.name)
+        jsonObject.addProperty("beer_type", beer.beer_type)
+        jsonObject.addProperty("description", beer.description)
+        scope.launch {
+            beerRepository.postBeer(jsonObject)
         }
     }
     fun cancelRequest() = coroutineContext.cancel()

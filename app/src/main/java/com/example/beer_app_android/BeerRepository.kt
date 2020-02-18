cdec7f8d3d6c6f28dfd841beb361bb21a420f732
July Slavhev <jul.slavchev.tues19@gmail.com>
2020-02-18 16:46:04 +0200

    Suspend function postBeer added in Beer Repository

--- app/src/main/java/com/example/beer_app_android/BeerRepository.kt
+++ app/src/main/java/com/example/beer_app_android/BeerRepository.kt
@@ -1,11 +1,20 @@
 package com.example.beer_app_android
 
+import com.google.gson.JsonObject
+
 class BeerRepository(private val apiInterface: BeersApiInterface) : BaseRepository() {
     suspend fun getBeer() : Beer? {
         return safeApiCall(
             //await the result of deferred type
             call = {apiInterface.getBeerAsync(1).await()},
-            error = "Error fetching beer"
+            error = "Error fetching a beer"
+        )
+    }
+
+    suspend fun postBeer(beer : JsonObject) {
+        safeApiCall(
+            call = {apiInterface.postBeerAsync(beer).await()},
+            error = "Error posting a beer"
         )
     }
 }

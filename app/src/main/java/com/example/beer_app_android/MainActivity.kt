e4692a7ad2e415b966f31dd224a04b7bef9bc9c9
July Slavhev <jul.slavchev.tues19@gmail.com>
2020-02-20 03:10:29 +0200

    Main Activity puts the list of beers into recyclerview.

--- app/src/main/java/com/example/beer_app_android/MainActivity.kt
+++ app/src/main/java/com/example/beer_app_android/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.beer_app_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var beerViewModel: BeerViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        beerViewModel = ViewModelProvider(this).get(BeerViewModel::class.java)
+        beerViewModel.getListOfBeers()
+        beerViewModel.beers.observe(this, Observer {beers ->
+            recycler_view_beers.also {
+                it.layoutManager = LinearLayoutManager(this)
+                it.adapter = BeersAdapter(beers)
+            }
+
+        })
     }
 }
